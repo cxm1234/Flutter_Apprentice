c8f1b6ecf8a40d3d661b2f493da993989a80e4fb
Mike <245413040@qq.com>
2024-06-26 18:50:19 +0800

    Grocery

--- fooderlich/lib/screens/grocery_screen.dart
+++ fooderlich/lib/screens/grocery_screen.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:fooderlich/models/grocery_manager.dart';
 import 'package:fooderlich/screens/empty_grocery_screen.dart';
+import 'package:provider/provider.dart';
 
 class GroceryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const EmptyGroceryScreen();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          // TODO 11: Present GroceryItemScreen
+
+        },
+      ),
+      body: buildGroceryScreen(),
+    );
+  }
+
+  Widget buildGroceryScreen() {
+    return Consumer<GroceryManager>(builder: (context, manager, child) {
+      if (manager.groceryItems.isNotEmpty) {
+        return Container();
+      } else {
+        return const EmptyGroceryScreen();
+      }
+    });
   }
 }
